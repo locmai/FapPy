5430e0781decf5061c849e17603e65dc96d778b7
Loc Mai <locmai@kms.com.vn>
2018-02-18 01:22:18 +0700

    add procfile and change setting

--- fappy/settings.py
+++ fappy/settings.py
@@ -125,6 +125,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'home'
