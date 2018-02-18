122b125623c7ffe787e6ca81f5d093851737f8af
Loc Mai <locmai@kms.com.vn>
2018-02-18 20:00:08 +0700

    change static

--- fappy/settings.py
+++ fappy/settings.py
@@ -124,9 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 LOGIN_URL = 'login'
