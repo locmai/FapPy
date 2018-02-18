6db7864ef737272d7e0abf791188d916488ba2ee
Loc Mai <locmai@kms.com.vn>
2018-02-18 18:30:48 +0700

    update procfile

--- fappy/settings.py
+++ fappy/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-from config import SECRET_KEY, get_allowed_hosts,get_database,get_debug_mode
+from config import SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
